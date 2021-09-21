3ec804f683fa03f54d9161df239795303405d1d8
stankovicjovan <stankovicjovan1994@gmail.com>
2021-09-22 00:54:14 +0200

    'updated-success-page'

--- process.php
+++ process.php
@@ -1,15 +1,17 @@
+
 <?php
-  if (isset($_POST['email']))  {
+  // if (isset($_POST['email']))  {
   
-    //Email information
-    $admin_email = "jovan@edgeledger.net";
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $phone = $_POST['phone'];
-    $message = $_POST['message'];
+  //   //Email information
+  //   $admin_email = "jovan@edgeledger.net";
+  //   $name = $_POST['name'];
+  //   $email = $_POST['email'];
+  //   $phone = $_POST['phone'];
+  //   $message = $_POST['message'];
     
-    //send email
-    mail($admin_email, "New Form Submission", $message . ' - ' . $phone, "From:" . $email);
+  //   //send email
+  //   mail($admin_email, "New Form Submission", $message . ' - ' . $phone, "From:" . $email);
     
-    header('Location: http://edgeledger.net/success.html');
-  }
+  //   header('Location: http://edgeledger.net/success.html');
+  // }
+  //  
